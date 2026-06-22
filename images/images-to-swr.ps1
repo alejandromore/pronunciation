452e2a7b7a988a76529d88cf7aed132a4dda98a7
alejandromore <alejandromore@gmail.com>
2026-06-21 20:49:59 -0500

    update image

--- images/images-to-swr.ps1
+++ images/images-to-swr.ps1
@@ -5,7 +5,8 @@
 #  Ubicacion esperada del script: images/ (raiz de los contextos de build).
 #
 #  Uso:
-#    .\images-to-swr.ps1 -SwrUser "la-south-2@HST3WARDF7OXIEACWCLV" -SwrPassword "cf6d50dd50ed623380cca26d6fa56047d30606cf1cb404310240963932a52525"
+#    .\images-to-swr.ps1 -SwrUser "la-south-2@AK" -SwrPassword "TOKEN"
+#
 #  Notas:
 #   - El build baja la base CUDA (pytorch/pytorch) y clona el upstream; necesita
 #     salida a internet durante el build.
